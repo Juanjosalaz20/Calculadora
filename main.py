7b53e992f53167e1a642161a241b8be8043e6072
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-04-29 20:00:48 -0500

    CLASE 29/04/25

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-from flask import Flask, request
-from operaciones import sumar
+from flask import Flask, request, render_template
+from operaciones import sumar, restar, multiplicar, dividir, random_1
+from livereload import Server
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '''
-<h1> Aplicacion de la calculadora </h1>
-<a href="/suma?num1=5&num2=4"> Ir a la pagina de suma </a>
-
-
-
-
-'''
+    return render_template('home.html')
 
 @app.route("/suma")
 def ruta_suma():
@@ -22,3 +16,43 @@ def ruta_suma():
         return 'Faltan datos'
 
     return f'la suma de num1 y num2 es {sumar(num1, num2)}'
+
+@app.route("/resta")
+def ruta_resta():
+    num1=request.args.get('num1',type=float)
+    num2=request.args.get('num2',type=float)
+    if num1 is None or num2 is None:
+        return 'Faltan datos'
+
+    return f'la resta de num1 y num2 es {restar(num1, num2)}'
+
+@app.route("/multiplicacion")
+def ruta_multiplicacion():
+    num1=request.args.get('num1',type=float)
+    num2=request.args.get('num2',type=float)
+    if num1 is None or num2 is None:
+        return 'Faltan datos'
+
+    return f'la multiplicacion de num1 y num2 es {multiplicar(num1, num2)}'
+
+@app.route("/division")
+def ruta_division():
+    num1=request.args.get('num1',type=float)
+    num2=request.args.get('num2',type=float)
+    if num1 is None or num2 is None:
+        return 'Faltan datos'
+
+    return f'la division de num1 y num2 es {dividir(num1, num2)}'
+
+@app.route("/aleatorio")
+def ruta_aleatorio():
+    num1=request.args.get('num1',type=float)
+    num2=request.args.get('num2',type=float)
+    if num1 is None or num2 is None:
+        return 'Faltan datos'
+
+    return f'el numero aleatorio entre num1 y num2 es {random_1(num1, num2)}'
+
+if __name__ == "__main__":
+    server = Server(app.wsgi_app)
+    server.serve()
--- operaciones.py
+++ operaciones.py
@@ -1,2 +1,22 @@
+import random
+
 def sumar(a,b):
     return a + b
+
+def restar(a,b):
+    return a-b
+
+def multiplicar(a,b):
+    return a*b
+
+def dividir(a,b):
+    if b == 0:
+        return 'No es posible dividir por cero'
+    return a/b
+
+def random_1 (a,b):
+    if a < 0:
+        return 'la variable a no puede ser menor que cero'
+    if a >= b:
+        return 'la variable a no puede ser mayor o igual a b'
+    return random.randint(a,b)
